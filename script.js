f153a16264cddb5921e451df05de2ebbb7adac33
Magdalena Nolde <magdalena@nolde.de>
2023-02-26 21:18:07 +0100

    next commit

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const schemeButtons = document.querySelectorAll(".scheme__button");
+const pledgeButtons = document.querySelectorAll(".pledge__button");
+const buttonsArray = [...schemeButtons];
 const mainButton = document.querySelector(".description__button");
 const modal = document.querySelector(".modal");
 const checkBoxes = document.querySelectorAll(".radio__input");
@@ -9,6 +11,10 @@ const hamburgerOpen = document.querySelector(".header__icon-mobile--open");
 const hamburgerClose = document.querySelector(".header__icon-mobile--close");
 const nav = document.querySelector(".nav");
 const menuWrapper = document.querySelector(".all__wrapper");
+const modalCloseBtn = document.querySelector(".modal__button");
+const endPage = document.querySelector(".final");
+const finalButton = document.querySelector(".final__button");
+buttonsArray.push(mainButton);
 
 // closing and opening menu
 
@@ -39,3 +45,39 @@ checkBoxes.forEach((item, i) => {
     schemes[i].classList.add("active");
   });
 });
+
+// opening the schemes modal
+
+// buttonsArray.forEach((button) => {
+// button.addEventListener("click", () => modal.showModal());
+// });
+//
+// closing the schemes modal
+
+modalCloseBtn.addEventListener("click", () => modal.close());
+
+// submitting pledge & opening the "thank you" modal & closing the scheme modal
+
+pledgeButtons.forEach((button) => {
+  const schemeInput = button.previousElementSibling.lastElementChild;
+  button.addEventListener("click", () => {
+    if (schemeInput.value === " " || schemeInput.value <= 0) {
+      alert("You have to enter a number greater than 0");
+    } else if (schemeInput.name === "medium" && schemeInput.value < 25) {
+      alert("you have to pledge min. 25 $");
+    } else if (schemeInput.name === "premium" && schemeInput.value < 75) {
+      alert("you have to pledge min. 75 $");
+    } else {
+      schemeInput.value = " ";
+      endPage.showModal();
+    }
+  });
+});
+
+// closing the "thank you" modal
+
+finalButton.addEventListener("click", () => endPage.close());
+
+
+
+ 
